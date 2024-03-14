255eefc44ec32c81932b6c433717c1ff7be73a2a
TKWebSE <writetechcode@yahoo.co.jp>
2024-03-14 23:59:59 +0900

    コメント

---
+++ AtCoder Beginner Contest 397/mySolutions/A - Thermometer_AnswerCopy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i = 0;i < (n);i++)
+
+int main() {
+  double x;
+  cin >> x;
+  if(38.0 <= x)cout << 1 << endl;
+  else if(37.5 <= x) cout << 2 << endl;
+  else cout << 3 << endl;
+  
+  return 0;
+}
