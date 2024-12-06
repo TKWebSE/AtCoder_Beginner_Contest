29ed57a8434b587062e8f0b2cbe9f94d484e3dd0
TKWebSE <writetechcode@yahoo.co.jp>
2024-12-06 19:41:37 +0900

    bfileAnswer

---
+++ 第19回 アルゴリズム実技検定(過去問)/C.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int N =sc.nextInt();
+      int BestNum = 0;
+      int BetterNum = 0;
+      String answer = "No"
+      
+      // 良い数字を判定
+      if(良い数字を判定){
+
+      }
+
+      // 良さそうな数字を判定
+      if(良さそうな数字を判定){
+        answer = "Yes";
+      }
+      System.out.println(answer);
+    }
+}
---
+++ 第19回 アルゴリズム実技検定(過去問)/example.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      // 標準入力を受け取る
+      int N = sc.nextInt();
+      int T = sc.nextInt();
+      int 1stAnswerCount = 0;
+      int 1stAnswerTime = 0;
+      // 標準入力と1位の正解問題数と最終正解時刻を計算する
+      for(int i = 0;i < N;i++){
+        
+        if(1stAnswerCount < ){
+          if(1stAnswerCount = ){
+            
+          }
+
+        }
+        }
+      }
+
+      // 答えを出す
+      int G = T * (A -) + ( - B)
+      System.out.println(T);
+    }
+}
