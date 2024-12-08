47aea4b92a53276373e8eaa4bd7bc67ab0221445
TKWebSE <writetechcode@yahoo.co.jp>
2024-12-09 03:01:32 +0900

    FixE

--- 第19回 アルゴリズム実技検定(過去問)/E.java
+++ 第19回 アルゴリズム実技検定(過去問)/E.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      
+      int lengthN = sc.nextInt();
+      String wordS = sc.next();
+
+      int x = 1;
+      int n = 0;
+      int i = 0;
+      int j = 0;
+      for(int loopi = 0;i < LengthN;i++){
+        // 2の条件を満たすnを探す
+          n = wordS.indexOf("/",x);
+          if(wordS.charAt(1 + n) == *){
+            i = n;
+          }
+          
+        
+        // 4の条件を満たすnを探す
+          if
+        // 6の処理
+        wordS = 
+        x = i
+      }
+
+      // 答えを出力
+      System.out.print(wordS);
     }
 }
