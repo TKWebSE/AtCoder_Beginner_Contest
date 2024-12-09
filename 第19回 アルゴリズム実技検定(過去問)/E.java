f1fc4500a12d4e5e34d64f57c7672a71e8e65249
TKWebSE <writetechcode@yahoo.co.jp>
2024-12-09 14:09:47 +0900

    FixE

--- 第19回 アルゴリズム実技検定(過去問)/E.java
+++ 第19回 アルゴリズム実技検定(過去問)/E.java
@@ -10,19 +10,23 @@ public class Main {
       int n = 0;
       int i = 0;
       int j = 0;
-      for(int loopi = 0;i < LengthN;i++){
-        // 2の条件を満たすnを探す
-          n = wordS.indexOf("/",x);
-          if(wordS.charAt(1 + n) == *){
-            i = n;
-          }
+      for(int loopi = 0;loopi < lengthN;loopi++){
+        // 2の条件を満たすnを探す(iを求める)
+          n = wordS.indexOf("/*",x);
+          System.out.println("n:" + n);
+          i = n;
           
-        
-        // 4の条件を満たすnを探す
-          if
+        // 4の条件を満たすnを探す(jを求める)
+          n = wordS.indexOf("*/",x);
+          System.out.println("n:" + n);
+          j = n;
+
         // 6の処理
-        wordS = 
-        x = i
+        if(i == -1 || j == -1){
+          break;
+        }
+        wordS = wordS.substring(x,i) + wordS.substring(j+2,lengthN);
+        x = i;
       }
 
       // 答えを出力
