62098de910766df615ebeeeda3287b9f56cc2120
TKWebSE <writetechcode@yahoo.co.jp>
2024-12-13 23:46:45 +0900

    AddH

--- 第19回 アルゴリズム実技検定(過去問)/E.java
+++ 第19回 アルゴリズム実技検定(過去問)/E.java
@@ -13,17 +13,17 @@ public class Main {
       for(int loopi = 0;loopi < lengthN;loopi++){
         // 2の条件を満たすnを探す(iを求める)
           n = wordS.indexOf("/*",x);
-          System.out.println(n);
+          // System.out.println(n);
           i = n;
           
         // 4の条件を満たすnを探す(jを求める)
           n = wordS.indexOf("*/",i);
-          System.out.println(n);
+          // System.out.println(n);
           j = n;
 
         // 6の処理
         if(i == -1 || j == -1 || j - i < 0){
-          System.out.println("loopi:" + loopi);
+          // System.out.println("loopi:" + loopi);
           if(loopi == 0){
             wordS = "";
           }
@@ -31,11 +31,12 @@ public class Main {
           break;
         }else{
           // ここでアウトバウンドが起きている
-          System.out.println("x:" + x);
-          System.out.println("i:" + i);
-          System.out.println("j:" + j);
-          System.out.println("j+2" + j + 2);
-          wordS = wordS.substring(x,i) + wordS.substring(j+2,lengthN);
+          // System.out.println("x:" + x);
+          // System.out.println("i:" + i);
+          // System.out.println("j:" + j);
+          // System.out.println("j+2" + j + 2);
+          //     System.out.println(wordS);
+          wordS = wordS.substring(x,i) + wordS.substring(j+2);
           x = i;
         }
       }
