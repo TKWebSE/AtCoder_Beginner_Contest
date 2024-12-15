93aceb58af045f6e2efcca860d27e757d3eb7122
TKWebSE <writetechcode@yahoo.co.jp>
2024-12-16 00:03:42 +0900

    WndF

--- 第19回 アルゴリズム実技検定(過去問)/F.java
+++ 第19回 アルゴリズム実技検定(過去問)/F.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
-    public static String DominoFallDown(String s) {
-      int N = sc.nextLine();
-      String[] domino = new String[N];
+    public static String DominoFallDown(Scanner sc) {
+      int N = sc.nextInt();
+      String X = sc.next();
+      String Y = sc.next();
+      HashMap<String,String> dominoMap = new HashMap<>();
+      
       // 情報を格納する
-      while(i < N){
-        
+      for(int i = 0;i < N;i++){
+        dominoMap.put(sc.next(),sc.next());
       }
 
-      //判定
-      if(){
-        return "Yes";
-      }else{
+      // 判定する
+      String key = X;
+      String value = "";
+      for(int i = 0;i < N;i++){
+        value = dominoMap.get(key);
+        if(value == null){
+          break;
+        }
+        if(value.equals(Y)){
+          return "Yes";
+        }
+        key = value;
+      }
+
+      // 見つからなかったとき
         return "No";
       }
-    }
+    
 
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      sc.close();
       System.out.print(DominoFallDown(sc));
     }
 }
