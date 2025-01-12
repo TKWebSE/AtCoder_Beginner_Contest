60a6f34fbc1e3f40ac7755b72c88f74896f32cb2
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-12 22:37:22 +0900

    LearnC++

--- AtCoder Programming Guide for beginners (APG4b)/3.04.構造体.cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.04.構造体.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 // EX24 - 時計の実装 
 // 以下に、24時間表記の時計構造体 Clock を定義する
-
+struct Clock{
 // Clock構造体のメンバ変数を書く
 //   int hour    時間を表す (0~23の値をとる)
 //   int minute  分を表す   (0~59の値をとる)
 //   int second  秒を表す   (0~59の値をとる)
-
+    int hour;
+    int minute;
+    int second;
 // メンバ関数 set の定義を書く
 //   関数名: set
 //   引数: int h, int m, int s (それぞれ時、分、秒を表す)
@@ -16,6 +18,11 @@ using namespace std;
 //   関数の説明:
 //     時・分・秒を表す3つの引数を受け取り、
 //     それぞれ、メンバ変数 hour, minute, second に代入する
+    void set(int h,int m,int s){
+      hour = h;
+      minute = m;
+      second = s;
+    }
 
 // メンバ関数 to_str の定義を書く
 //   関数名: to_str
@@ -26,6 +33,9 @@ using namespace std;
 //     時刻の文字列は次のフォーマット
 //     "HH:MM:SS"
 //     HH、MM、SSはそれぞれ時間、分、秒を2桁で表した文字列
+    string to_str(){
+      cout << hour <<":" << minute <<  ":" << second << endl;
+    }
 
 // メンバ関数 shift の定義を書く
 //   関数名: shift
@@ -37,7 +47,21 @@ using namespace std;
 //     diff_second の値が正の場合、時刻を進める
 //     diff_second の値は -86400 ~ 86400 の範囲を取とりうる
 
-
+    void shift(int diff_second){
+      
+      if(0 > diff_second){
+        // 時刻をマイナスする
+        hour = hour - diff_second / 360;
+        minute = minute - diff_second / 60;
+        second = second - diff_second % 60;
+      }else{
+        // 時刻をプラスする
+        hour = hour + diff_second / 360;
+        minute = minute + diff_second / 60;
+        second = second + diff_second % 60;
+      }
+    }
+};
 // -------------------
 // ここから先は変更しない
 // -------------------
