c916fb6c9afc4cdfacc2ed4c502034395ce7803d
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-17 00:48:38 +0900

    BAdd

--- AtCoder Programming Guide for beginners (APG4b)/3.05.ビット演算 .cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.05.ビット演算 .cpp
@@ -1,35 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
+// EX25 - 集合の操作 
+// 各操作を行う関数を実装する
 
-int main () {
-  int N, K;
-  cin >> N >> K;
-  vector<int> A(N);
+// AとBに共通して含まれる要素からなる集合を返す
+bitset<50> intersection(bitset<50> A, bitset<50> B) {
+}
+// AとBのうち少なくとも一方に含まれる要素からなる集合を返す
+bitset<50> union_set(bitset<50> A, bitset<50> B) {
+}
+// AとBのうちどちらか一方にだけ含まれる要素からなる集合を返す
+bitset<50> symmetric_diff(bitset<50> A, bitset<50> B) {
+}
+// Aから値xを除く
+bitset<50> subtract(bitset<50> A, int x) {
+}
+// Aに含まれる要素に1を加える(ただし、要素49が含まれる場合は0になるものとする)
+bitset<50> increment(bitset<50> A) {
+}
+// Aに含まれる要素から1を引く(ただし、要素0が含まれる場合は49になるものとする)
+bitset<50> decrement(bitset<50> A) {
+}
+
+// Sに値xを加える
+bitset<50> add(bitset<50> S, int x) {
+  S.set(x, 1);  // xビット目を1にする
+  return S;
+}
+
+// 集合Sの内容を昇順で出力する(スペース区切りで各要素の値を出力する)
+void print_set(bitset<50> S) {
+  vector<int> cont;
+  for (int i = 0; i < 50; i++) {
+    if (S.test(i)) {
+      cont.push_back(i);
+    }
+  }
+  for (int i = 0; i < cont.size(); i++) {
+    if (i > 0) cout << " ";
+    cout << cont.at(i);
+  }
+  cout << endl;
+}
+
+// これより下は書き換えない
+
+int main() {
+  bitset<50> A, B;
+  int N;
+  cin >> N;
   for (int i = 0; i < N; i++) {
-    cin >> A.at(i);
+    int x;
+    cin >> x;
+    A = add(A, x);
+  }
+  int M;
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    int x;
+    cin >> x;
+    B = add(B, x);
   }
 
-  bool ans = false;
+  // 操作
+  string com;
+  cin >> com;
 
-  // すべての選び方を試して、総和がKになるものがあるかを調べる
-  for (int tmp = 0; tmp < (1 << 20); tmp++) {
-    bitset<20> s(tmp);  // 最大20個なので20ビットのビット列として扱う
-
-    // ビット列の1のビットに対応する整数を選んだとみなして総和を求める
-    int sum = 0;
-    for (int i = 0; i < N; i++) {
-      if (s.test(i)) {
-        sum += A.at(i);
-      }
-    }
-    if (sum == K) {
-      ans = true;
-    }
-  }
-
-  if (ans) {
-    cout << "YES" << endl;
-  } else {
-    cout << "NO" << endl;
+  if (com == "intersection") {
+    print_set(intersection(A, B));
+  } else if (com == "union_set") {
+    print_set(union_set(A, B));
+  } else if (com == "symmetric_diff") {
+    print_set(symmetric_diff(A, B));
+  } else if (com == "subtract") {
+    int x;
+    cin >> x;
+    print_set(subtract(A, x));
+  } else if (com == "increment") {
+    print_set(increment(A));
+  } else if (com == "decrement") {
+    print_set(decrement(A));
   }
 }
