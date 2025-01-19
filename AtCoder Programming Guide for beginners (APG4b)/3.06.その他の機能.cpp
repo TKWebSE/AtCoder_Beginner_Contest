c222c3aaa8683f17e057df8ed85b6180b22ca01a
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-19 20:37:19 +0900

    LearnC++

--- AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
   int N =0;
   cin >> N;
+  vector<string> codes(N);
 
   // 命令を受け取る
-
+  for(int i = 0;i < N;i++){
+    cin >> codes.at(i); 
+  }
   // 計算する
 
   // 答えを出力する
