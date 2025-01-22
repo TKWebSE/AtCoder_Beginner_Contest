0852616cbebbba2e98c25676ab5610e0d0601375
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-23 00:22:09 +0900

    LearnC++

--- AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
@@ -1,62 +1,77 @@
-#include <bits/stdc++.h>
-using namespace std;
+// 問題文の形式でvec値を出力
+void print_vec(vector<int> vec)
 
-// EX26 - 電卓を作ろう3 
+// 変数名を読み取りイコールを読み飛ばす
+string read_name()
 
-// 整数の変数を宣言
-int declaration_int(int x){
-  cin >> 
-}
+// int式の項を1つ読み取る。
+// 数字ならその値を返し、そうでないなら変数として解釈し変数の値を返す
+// var_int : intの変数を保持するmap
+int read_int(map<string, int> &var_int)
 
-// 整数の値を出力
-void print_int(int x){
-  cout << x << endl;
-}
+// int式全体を読み取って計算する
+// var_int : intの変数を保持するmap
+int calc_int(map<string, int> &var_int)
 
-// 配列の変数を宣言
-void declaration_vec(vector<int> vec){
-  cin >> 
-  
-}
+// vec値を読み取る
+// 最初の"["は読み取ってある前提であることに注意
+// var_int : intの変数を保持するmap
+vector<int> read_vec_val(map<string, int> &var_int)
 
-// 配列の値を出力
-void print_vec(vector<int> vec) {
-  cout << "[ ";
-  for (int i = 0; i < vec.size(); i++) {
-    cout << vec.at(i) << " ";
-  }
-  cout << "]" << endl;
-}
+// vec式の項を1つ読み取る
+// vec値ならその値を返し、そうでないなら変数として解釈し変数の値を返す
+// var_int : intの変数を保持するmap
+// var_vec : vecの変数を保持するmap
+vector<int> read_vec(map<string, int> &var_int, map<string, vector<int>> &var_vec)
+
+// vec式全体を読み取って計算する
+// var_int : intの変数を保持するmap
+// var_vec : vecの変数を保持するmap
+vector<int> calc_vec(map<string, int> &var_int, map<string, vector<int>> &var_vec)
 
 
 int main() {
-  int N =0;
+
+  // 命令の行数を取得
+  int N;
   cin >> N;
-  vector<string> codes(N);
 
-  // 命令を受け取る
-  for(int i = 0;i < N;i++){
-    cin >> x;
-    while(x == ";"){
-      if(x == "int"){
-        
-      }
-      if(x == "print_int"){
+  map<string, int> var_int; // intの変数を管理するmap
+  map<string, vector<int>> var_vec; // vectorの変数を管理するmap
 
-      }
-      if(x == "vec"){
+  // 行数分の処理
+  for (int i = 0; i < N; i++) {
 
-      }
-      if(x == "print_vec"){
+    // 命令を受け取る
+    string s;
+    cin >> s;
 
-      }
+    // int命令の処理
+    if (s == "int") {
+      // 変数名を読み取る
+      string name = read_name();
+      // 右辺の式を計算して変数に代入
+      var_int[name] = calc_int(var_int);
+    }
 
-      cin >> x;
+    // vec命令の処理
+    if (s == "vec") {
+      // 変数名を読み取る
+      string name = read_name();
+      // 右辺の式を計算して変数に代入
+      var_vec[name] = calc_vec(var_int, var_vec);
+    }
+
+    // print_int命令の処理
+    if (s == "print_int") {
+      // 式を計算して出力
+      cout << calc_int(var_int) << endl;
+    }
+
+    // print_vec命令の処理
+    if (s == "print_vec") {
+      // 式を計算して出力
+      print_vec(calc_vec(var_int, var_vec));
+    }
   }
-  }
-  // 計算する
-  
-  
-  // 答えを出力する
-  cout << "Hello, world!" << endl;
 }
