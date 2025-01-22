8f931dc9c3b072978144e898bce3427ab13c4f38
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-22 22:57:33 +0900

    LearnC++

--- AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// EX26 - 電卓を作ろう3 
+
+// 整数の変数を宣言
+int declarationInt(int x){
+
+}
+
+// 整数の値を出力
+void printInt(int x){
+
+}
+
+// 配列の変数を宣言
+void declarationVec(vector ){
+
+}
+
+// 配列の値を出力
+void print_vec(vector<int> vec) {
+  cout << "[ ";
+  for (int i = 0; i < vec.size(); i++) {
+    cout << vec.at(i) << " ";
+  }
+  cout << "]" << endl;
+}
+
+
 int main() {
   int N =0;
   cin >> N;
