eff96ad50e62ade8486c8c6a2faf9de29a8e51e7
TKWebSE <writetechcode@yahoo.co.jp>
2025-01-27 21:29:19 +0900

    LearnC++

--- AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
+++ AtCoder Programming Guide for beginners (APG4b)/3.06.その他の機能.cpp
@@ -21,11 +21,15 @@ string read_name(){
 // int式の項を1つ読み取る。
 // 数字ならその値を返し、そうでないなら変数として解釈し変数の値を返す
 // var_int : intの変数を保持するmap
-int read_int(map<string, int> &var_int)
+int read_int(map<string, int> &var_int){
+  
+}
 
 // int式全体を読み取って計算する
 // var_int : intの変数を保持するmap
-int calc_int(map<string, int> &var_int)
+int calc_int(map<string, int> &var_int){
+  
+}
 
 // vec値を読み取る
 // 最初の"["は読み取ってある前提であることに注意
