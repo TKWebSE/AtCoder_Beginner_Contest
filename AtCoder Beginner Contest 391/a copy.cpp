1dbd9c339df6bdad4ccb4afe06b9f9d487ddda13
TKWebSE <writetechcode@yahoo.co.jp>
2025-02-01 16:49:28 +0900

    LearnC++

---
+++ AtCoder Beginner Contest 391/a copy.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long X=0;
+  long answer = 1;
+  int kaijo = 1;
+  cin >> X;
+
+  while(X != answer){
+    answer = 1;
+    for(int i = 2;i <= 20;i++){
+      answer = answer * i;
+      // cout << i << endl;
+      // cout << X << ":" <<answer << endl;
+      // cout << "--------" << endl;
+      if(X = answer) cout i endl;
+    }
+  }
+
+}
