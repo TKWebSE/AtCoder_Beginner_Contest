519a6315a35f008d7e4939af3bbe37c10817ba1c
TKWebSE <writetechcode@yahoo.co.jp>
2025-02-03 17:09:16 +0900

    LearnC++

---
+++ AtCoder Beginner Contest 391/B- Seek Grid-answercopy.cpp
@@ -0,0 +1,28 @@
+https://atcoder.jp/contests/abc391/tasks/abc391_b
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N,M;
+  cin >> N >> M;
+  vector<string> S(N),T(M);
+  for(auto& x : S) cin >> x;
+  for(auto& x : T) cin >> x;
+
+  for(int a = 0;a <= N - M;++a){
+    for(int b = 0;b <= N - M;++b){
+      bool ok = true;
+      for(int i = 0;i < M;++i){
+        for(int j = 0;j<M;++j){
+          if(S[a+i][b+j] != T[i][j]){
+            ok = false;
+          }
+        }
+      }
+      if(ok){
+        cout << a + 1 << " " << b + 1 <<endl;
+      }
+    }
+  }
+}
