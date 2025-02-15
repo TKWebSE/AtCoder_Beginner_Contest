9f2768b358195c1a229efc750a49306432805448
TKWebSE <writetechcode@yahoo.co.jp>
2025-02-15 21:29:20 +0900

    LearnC++

--- AtCoder Beginner Contest 393/A - Poisonous Oyster.cpp
+++ AtCoder Beginner Contest 393/A - Poisonous Oyster.cpp
@@ -2,9 +2,32 @@
 using namespace std;
 
 int main() {
-  int N = 0;
-  cin >> N;
+  string Takahashi = "";
+  string Aoki = "";
+  cin >> Takahashi >> Aoki;
+  const string SICK = "sick";
+  const string FINE = "fine";
+  int answer = 0;
 
+  // 1
+  if(Takahashi == SICK && Aoki == SICK){
+    answer = 1;
+  }
 
-  
+  // 2
+  if(Takahashi == SICK && Aoki != SICK){
+    answer = 2;
+  }
+
+  // 3
+  if(Takahashi != SICK && Aoki == SICK){
+    answer = 3;
+  }
+
+  // 4
+  if(Takahashi != SICK && Aoki != SICK){
+    answer = 4;
+  }
+
+  cout << answer << endl;
 }
