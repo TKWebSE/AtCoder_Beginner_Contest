e9530df2cef7d4b531a35b13d6202a538c1af8e0
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-03 20:51:16 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 395/C△ - Shortest Duplicate Subarray.cpp
+++ AtCoder Beginner Contest 395/C△ - Shortest Duplicate Subarray.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ACにならなかった。実行速度超過。TLE
 int main() {
   int ans = -1;
   int N = 0;
   int count = 1;
   cin >> N;
   vector<int> A(N);
-
+  // 初期値設定
   for(auto& a:A)cin >> a;
   
+  // 判定ロジック
   for(int i = 0;i < N;i++){
     int x = A.at(i);
     count = 1;
