3f7532d4f0b221ac6bbdb47217b7bc3866f21c22
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-05 22:08:43 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 395/C - Shortest Duplicate Subarray_AnswerCopy.cpp
+++ AtCoder Beginner Contest 395/C - Shortest Duplicate Subarray_AnswerCopy.cpp
@@ -5,6 +5,26 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+
+  vector<int> cnt(1000004);
+  int mult =0;
+
+  const int INF = 1001001001;
+  int ans = INF;
+  int r =0;
+  rep(l,n){
+    while(r < n && mult == 0){
+      cnt[a[r]]++;
+      r++;
+    }
+    if(mult == 0) break;
+    ans = min(ans,r-l);
+    if(cnt[a[l]] == 2) mult--;
+    cnt[a[l]]--;
+  }
+  if(ans == INF) ans = -1;
+  cout << ans << endl;
   return 0;
 }
