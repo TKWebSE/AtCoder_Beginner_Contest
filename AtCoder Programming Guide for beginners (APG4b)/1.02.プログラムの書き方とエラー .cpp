4ef8e1dad40eb13b0f5661cc2d70c7b79ada16b1
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-11 22:52:26 +0900

    LearnAtCoder

--- AtCoder Programming Guide for beginners (APG4b)/1.02.プログラムの書き方とエラー .cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-	// EX2 - エラーの修正
-int main() {
-  cout << "いつも" << 2525 << endl;
-  cout << "AtCoderくん"<< endl;
-}
--- AtCoder Programming Guide for beginners (APG4b)/1.03.四則演算と優先順位.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// EX3 - 計算問題
-int main() {
-  cout << 0.5 * 100 * (100 + 1) << endl;
-}
