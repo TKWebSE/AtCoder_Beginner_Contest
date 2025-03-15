3c3e3e61f832211f24c5672cd1b90979e2bbfef9
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-15 21:05:09 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 397/explanations/A - Thermometer.cpp
+++ AtCoder Beginner Contest 397/explanations/A - Thermometer.cpp
@@ -3,10 +3,17 @@ using namespace std;
 #define rep(i,n) for(int i = 0;i < (n);i++)
 
 int main() {
-  string ans = "";
-  int n = 0;
-  cin >> n;
+  int ans;
+  int x = 0;
+  cin >> x;
 
+  if(x >= 38){
+    ans = 1;
+  }else if(x >= 37.5){
+    ans = 2;
+  }else {
+    ans = 3;
+  }
 
   cout << ans << endl;
   return 0;
