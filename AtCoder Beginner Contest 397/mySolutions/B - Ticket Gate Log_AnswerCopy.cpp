3bb2ae470cc8c427fb2d8a6e7fe4cf2ee9bedd35
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-16 22:28:27 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 397/mySolutions/B - Ticket Gate Log_AnswerCopy.cpp
+++ AtCoder Beginner Contest 397/mySolutions/B - Ticket Gate Log_AnswerCopy.cpp
@@ -3,9 +3,13 @@ using namespace std;
 #define rep(i,n) for(int i = 0;i < (n);i++)
 
 int main() {
-  string ans = "";
-  int n = 0;
-  cin >> n;
+  string s = "";
+  cin >> s;
+  int ans = 0;
+  char expect = 'i';
+  for(char c :s){
+    if(c != expect) ans++;
+  }
 
 
   cout << ans << endl;
