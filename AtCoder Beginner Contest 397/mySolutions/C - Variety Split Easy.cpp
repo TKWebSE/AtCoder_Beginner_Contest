840732b7bc96355d1727f4ecbea87b767e711153
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-27 00:41:05 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 397/mySolutions/C - Variety Split Easy.cpp
+++ AtCoder Beginner Contest 397/mySolutions/C - Variety Split Easy.cpp
@@ -8,9 +8,22 @@ int main() {
   map<int,int> right;
   int n = 0;
   cin >> n;
-  vector<int,int> a(n);
+  vector<int> a(n);
   for(auto& x : a) cin >> x;
   
+  // rightに数字の出現回数と数字を全て記録する
+  rep(i,n){
+    if(right[a[i]].count == 0){
+      right[a[i]] = 1;
+    }else{
+      right[a[i]]++;
+    }
+  }
+
+  // iを一文字ずつずらして右から左に数字を移動させて、maxを更新する
+  rep(i,n){
+
+  }
   
 
   cout << ans << endl;
