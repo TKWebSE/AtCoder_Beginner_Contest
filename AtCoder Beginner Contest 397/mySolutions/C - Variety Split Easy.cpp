5625382ef02d436012fe1e0511229bd730d7fd9e
TKWebSE <writetechcode@yahoo.co.jp>
2025-03-27 00:35:40 +0900

    LearnAtCoder

--- AtCoder Beginner Contest 397/mySolutions/C - Variety Split Easy.cpp
+++ AtCoder Beginner Contest 397/mySolutions/C - Variety Split Easy.cpp
@@ -4,36 +4,15 @@ using namespace std;
 
 int main() {
   int ans = 0;
+  map<int,int> left;
+  map<int,int> right;
   int n = 0;
   cin >> n;
-  vector<int> a(n);
-
-  for(auto& x :a)cin >> x;
-
-  // 分割ロジック
-  for(int i = 1;i < n-1;i++){
-    map<int,int> before;
-    map<int,int> after;
-
-    // beforeCountロジック
-    for(int j = 0;j < i;j++){
-      if(!(before.count(a.at(j)))){
-        before[a.at(j)] = 1;
-      }
-    }
-
-    // afterCountロジック
-    for(int j = i;j < n-1;j++){
-      if(!(after.count(a.at(j)))){
-        after[a.at(j)] = 1;
-      }
-    }
-    int beforeSize = before.size();
-    int afterSize = after.size();
-    //最大値を設定する
-    ans = max(ans,beforeSize + afterSize);
-  }
+  vector<int,int> a(n);
+  for(auto& x : a) cin >> x;
   
+  
+
   cout << ans << endl;
   return 0;
 }
