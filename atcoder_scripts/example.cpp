c3fb100157138c95d47ef9d725a9bc7c690572cb
TKWebSE <writetechcode@yahoo.co.jp>
2025-04-01 00:07:44 +0900

    atcoder

--- atcoder_scripts/example.cpp
+++ atcoder_scripts/example.cpp
@@ -1,13 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for(int i = 0;i < (n);i++)
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+void solve() {
+    // 問題ごとの実装を書く部分
+}
 
 int main() {
-  string ans = "";
-  int n = 0;
-  cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-
-  cout << ans << endl;
-  return 0;
+    solve();
+    return 0;
 }
