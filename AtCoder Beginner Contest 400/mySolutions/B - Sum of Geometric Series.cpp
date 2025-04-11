c2bc592d1cce98d6dfd8e60bc9a8bc1eca19fb0a
TKWebSE <writetechcode@yahoo.co.jp>
2025-04-11 23:59:59 +0900

    コメント

---
+++ AtCoder Beginner Contest 400/mySolutions/B - Sum of Geometric Series.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+void solve() {
+    // 問題ごとの実装を書く部分
+    const ll inf = 1000000000;
+    int n,m = 0;
+    ll ans = 0;
+    cin >> n >> m;
+    // ll num = 0;
+    for(int i = 0; i < m+1;i++){
+        ans += (ll)pow(n,i);
+    }
+    if(ans > inf || ans < 0){
+        cout << "inf" << endl;
+    }else{
+        cout << ans << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
