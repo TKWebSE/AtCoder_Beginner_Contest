0bc6fae94ef2a07777ffc3ee36f81316aad77a3d
TKWebSE <writetechcode@yahoo.co.jp>
2025-12-15 23:59:59 +0900

    コメント

--- AtCoder Beginner Contest 394/explanations/B - cat_AnswerCopy.cpp
+++ AtCoder Beginner Contest 394/explanations/B - cat_AnswerCopy.cpp
@@ -3,6 +3,15 @@ using namespace std;
 #define rep(i,n) for(int i = 0;i < (n);++i)
 
 int main() {
-  int n = 0;
-
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  rep(i,n) cin >> s[i];
+  sort(s.begin(),s.end(),{&}(string a,string b){
+    return a.size() < b.size();
+  });
+  string ans;
+  for(string ns : s) ans += ns;
+  cout << ans << endl;
+  return 0;
 }
